513d596db1b9c25b13828e11483c01f1e52d8dc3
BenjaminSzodo <benjaminszodo@gmail.com>
2023-05-05 10:26:19 -0300

    version 5 "working?"

---
+++ Server/src/controllers/postUser.js
@@ -0,0 +1,15 @@
+const {User} = require('..DB_connection/')
+
+
+const postUser = async(req,res) => {
+    const {email,password} = req.body;
+    try {
+        cons
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    postUser,
+};
