29139ecffb83a40c10ecb03f15ea6e8ddc343b7d
BenjaminSzodo <benjaminszodo@gmail.com>
2023-05-15 16:22:53 -0300

    version pre final

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -2,9 +2,6 @@ import  axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const URL_BASE = 'https://be-a-rym.up.railway.app/api/character';
-const API_KEY = '8b6bd1748044.3c7b319f29bbb0922084';
-
 export default function Deatil() {
     const {id} = useParams();
     const [character,setCharacter] = useState({});
